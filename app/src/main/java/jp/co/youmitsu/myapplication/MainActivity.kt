743fd907d99319046dd5e95e926a423416970349
yu mitsuhori <ymbullseye@gmail.com>
2019-04-09 01:39:28 +0900

    first_commit

---
+++ app/src/main/java/jp/co/youmitsu/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.co.youmitsu.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
