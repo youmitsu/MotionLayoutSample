bfdb67a19aed372b746dead8aad178a30b6fb4bb
yu mitsuhori <ymbullseye@gmail.com>
2019-04-11 01:31:39 +0900

    CustomView化

--- app/src/main/java/jp/co/youmitsu/myapplication/CustomMotionEditTextLayout.kt
+++ app/src/main/java/jp/co/youmitsu/myapplication/CustomMotionEditTextLayout.kt
@@ -1,18 +1,27 @@
 package jp.co.youmitsu.myapplication
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.constraint.motion.MotionLayout
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import jp.co.youmitsu.myapplication.databinding.LayoutCustomMotionEdittextBinding
 
 class CustomMotionEditTextLayout : MotionLayout {
+
+    private val binding: LayoutCustomMotionEdittextBinding
+    val title: String = ""
+    val value: String = ""
 
     constructor(context: Context) : this(context, null)
 
     constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
 
     constructor(context: Context, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle) {
-        LayoutInflater.from(context).inflate(R.layout.activity_test, this, true)
+        val inflater = LayoutInflater.from(context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.layout_custom_motion_edittext, this, true)
+        binding.title = title
+        binding.value = value
     }
 
 }
--- app/src/main/java/jp/co/youmitsu/myapplication/MainActivity.kt
+++ app/src/main/java/jp/co/youmitsu/myapplication/MainActivity.kt
@@ -1,11 +1,7 @@
 package jp.co.youmitsu.myapplication
 
-import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.SpannableStringBuilder
-import android.view.inputmethod.InputMethodManager
-import kotlinx.android.synthetic.main.activity_test.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,29 +11,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        nickname_value.setOnTouchListener { v, event ->
-            motion_layout.transitionToEnd()
-            edit_text.requestFocus()
-            edit_text.setSelection(edit_text.length())
-            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            imm.showSoftInput(edit_text, InputMethodManager.SHOW_IMPLICIT)
-            true
-        }
-        edit_text.setOnFocusChangeListener { v, hasFocus ->
-            text = (edit_text.text as SpannableStringBuilder).toString()
-            edit_text.setText(text)
-            nickname_value.text = if (text.isEmpty()) "--" else text
-            if (!hasFocus) {
-                motion_layout.transitionToStart()
-                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                inputMethodManager.hideSoftInputFromWindow(motion_layout.windowToken, 0)
-            }
-        }
-        motion_layout.setOnTouchListener { v, event ->
-            edit_text.clearFocus()
-            true
-        }
-        nickname_value.text = if (text.isEmpty()) "--" else text
-        edit_text.setText(text)
+//        nickname_value.setOnTouchListener { v, event ->
+//            motion_layout.transitionToEnd()
+//            edit_text.requestFocus()
+//            edit_text.setSelection(edit_text.length())
+//            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//            imm.showSoftInput(edit_text, InputMethodManager.SHOW_IMPLICIT)
+//            true
+//        }
+//        edit_text.setOnFocusChangeListener { v, hasFocus ->
+//            text = (edit_text.text as SpannableStringBuilder).toString()
+//            edit_text.setText(text)
+//            nickname_value.text = if (text.isEmpty()) "--" else text
+//            if (!hasFocus) {
+//                motion_layout.transitionToStart()
+//                val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//                inputMethodManager.hideSoftInputFromWindow(motion_layout.windowToken, 0)
+//            }
+//        }
+//        motion_layout.setOnTouchListener { v, event ->
+//            edit_text.clearFocus()
+//            true
+//        }
+//        nickname_value.text = if (text.isEmpty()) "--" else text
+//        edit_text.setText(text)
     }
 }
